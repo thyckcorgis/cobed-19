f6120178f7c5aacfd52252c13b8ca1c190791a49
charlesancheta <cancheta@ualberta.ca>
2020-05-06 05:54:08 -0600

    bruh

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
+import styles from './App.module.css';
+import cobedImage from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  };
+
+  async componentDidMount() {
+    this.handleCountryChange();
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img src={cobedImage} className={styles.image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker onCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
